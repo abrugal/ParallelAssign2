5009b6589317d1489d2cafdf90aab050a0174227
EB <estebancbrugal@gmail.com>
2023-02-23 00:41:35 -0500

    Add timer and comments

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -9,6 +9,7 @@
 
 
 using namespace std;
+using namespace std::chrono;
 
 
 std::atomic<int> counter = 0;
@@ -17,7 +18,9 @@ mutex m;
 
 void labyrinth(int index) {
 	bool hasSeenCupcake = false;
-	while (counter < 100) {
+	while (counter < 1000) {
+
+		// Thread 0 checks if there's a cupcake, which means that someone new entered the room
 		if (index == 0) {
 			m.lock();
 			if (cupcakeFlag == 1) {
@@ -27,6 +30,7 @@ void labyrinth(int index) {
 			m.unlock();
 		}
 		else {
+			// Only add the cupcake if it's your first time seeing an empty plate
 			m.lock();
 			if (cupcakeFlag == 0 && !hasSeenCupcake) {
 				cupcakeFlag = 1;
@@ -40,19 +44,23 @@ void labyrinth(int index) {
 int main()
 {
 	vector<thread> threads;
-	for (int i = 0; i < 100; i++) {
+
+	auto start = high_resolution_clock::now();
+	for (int i = 0; i < 1000; i++) {
 		threads.emplace_back(thread(labyrinth, i));
 	}
 
-	for (int i = 0; i < 100; i++) {
+	for (int i = 0; i < 1000; i++) {
 		threads[i].join();
 	}
 
-	if (counter >= 100) {
-		cout << "Everyone entered the labyrinth at least once." << endl;
+	auto stop = high_resolution_clock::now();
+	std::chrono::duration<double> diff = stop - start;
+
+	if (counter >= 1000) {
+		cout << "Everyone entered the labyrinth at least once in " << diff.count() << " seconds" << endl;
 	}
 	else {
 		cout << "Everyone didn't enter the labyrinth" << endl;
 	}
-	cout << counter << endl;
 }
